cfd505323a4d60d28aec29b3219a5296dc785f86
Gengjinxian <34221904+Gengjinxian@users.noreply.github.com>
2020-05-22 14:33:48 +0800

    2020-05-22 14:33

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,5 +1,9 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+    verbose_name = "用户管理"
+
+    def ready(self):
+        import users.signals
